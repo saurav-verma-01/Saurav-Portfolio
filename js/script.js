726e2d83742c68346d44022681ce0ba85255f1ab
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-04-30 21:01:04 +0530

    website complete from design

--- js/script.js
+++ js/script.js
@@ -24,3 +24,27 @@ const changeTheme = () => {
 };
 
 $themeBtn.addEventListener("click", changeTheme);
+
+// Tab
+
+const $tabBtn = document.querySelectorAll("[data-tab-btn]");
+
+let [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+
+let [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach((item) => {
+  item.addEventListener("click", function () {
+    lastActiveTab.classList.remove("active");
+    lastActiveTabBtn.classList.remove("active");
+
+    const $tabContent = document.querySelector(
+      `[data-tab-content="${item.dataset.tabBtn}"]`
+    );
+    $tabContent.classList.add("active");
+    this.classList.add("active");
+
+    lastActiveTab = $tabContent;
+    lastActiveTabBtn = this;
+  });
+});
