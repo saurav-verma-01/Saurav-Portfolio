5d30292d5f19ed58de9622c8c97e0de1d9b0d07e
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-04-30 17:08:33 +0530

    skill list in making

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-/**
- * @copyright codewithsadee 2023
- * @author Sadee <codewithsadee24@gmail.com>
- */
-
 "use strict";
+
+// Light and Dark Mode
+
+const $themeBtn = document.querySelector("[data-theme-btn]");
+
+const $HTML = document.documentElement;
+
+let isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
+
+console.log(isDark);
+
+if (sessionStorage.getItem("theme")) {
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  $HTML.dataset.theme = isDark ? "dark" : "Light";
+  sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+const changeTheme = () => {
+  $HTML.dataset.theme =
+    sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+  sessionStorage.setItem("theme", $HTML.dataset.theme);
+};
+
+$themeBtn.addEventListener("click", changeTheme);
